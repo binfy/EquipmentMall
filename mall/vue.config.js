7c4a99153ed75946ea8eb454ab10d3a813308210
jiyunkeji <jykjgit@sina.com>
2020-02-24 13:44:00 +0800

    大师傅

--- mall/vue.config.js
+++ mall/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  publicPath: '/mall/dist/',
+  publicPath: './',
   outputDir: 'dist',
+  filenameHashing: true,
     pages: {
         index: {
           // page 的入口
@@ -12,6 +13,9 @@ module.exports = {
           // 当使用 title 选项时，
           // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
           title: '装备市场',
+           // 在这个页面中包含的块，默认情况下会包含
+           // 提取出来的通用 chunk 和 vendor chunk。
+          chunks: ['chunk-vendors', 'chunk-common', 'index']
         },
       
       }
